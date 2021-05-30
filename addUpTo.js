8cc6c1df3ba8255f2c8325863dbd2ca0ecba446e
prateeksarangi <sarangiprateek80@gmail.com>
2021-05-30 22:56:39 +0530

    Big O notation added

---
+++ addUpTo.js
@@ -0,0 +1,21 @@
+// Approach 1
+
+let addUpTo = (n) => {
+    let total = 0;
+
+    for (let i = 1; i <= n; i++) {
+        total += i;
+    }
+
+    return total;
+}
+
+console.log(addUpTo(6));
+
+// Approach 2
+
+let addUpTo2 = (n) => {
+    return n*(n+1)/2;
+}
+
+console.log(addUpTo2(6));
